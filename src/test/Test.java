eb0422a68bcd1450fb023b4acf54e4b69d5bcdf9
yiqian <yiq940401@163.com>
2019-04-11 16:01:20 +0800

    提交修改

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,13 +3,14 @@ package test;
 import java.util.Date;
 
 /**
- * Created by Administrator on 2019/4/1.
+ * Created by Administrator on 2019/4/1. ²âÊÔÐÞ¸Ä
  */
 class Something {
 
     private static void main(String[] something_to_do){
 
         System.out.println("Dosomething ...");
+	System.out.println("Dosomething 2 ...");
 
     }
 
